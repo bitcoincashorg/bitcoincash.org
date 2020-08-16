4102bb9728b7d6f74f78dc175d2bb3c05b064b0d
deadalnix <deadalnix@gmail.com>
2020-08-16 23:53:05 +0200

    Rework gatsby-config.js (#517)

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -1,60 +1,57 @@
 module.exports = {
   siteMetadata: {
-    title: `Bitcoincash.org`,
-    description: `Money built for you—not banks`,
-    author: `@gatsbyjs`,
+    title: 'Bitcoincash.org',
+    description: 'Money built for you—not banks',
+    author: '@gatsbyjs',
     themeColours: {
-      primary_dark: `#040c3c`,
-      primary_light: `#212c6e`,
-      secondary_dark: `#bb5a00`,
-      secondary_light: `#ff8d00`,
-      background_dark: `#f3f3f3`
+      primary_dark: '#040c3c',
+      primary_light: '#212c6e',
+      secondary_dark: '#bb5a00',
+      secondary_light: '#ff8d00',
+      background_dark: '#f3f3f3',
     }
   },
-  assetPrefix: "/v2",
+  assetPrefix: '/v2',
   plugins: [
-  {
-    resolve: `gatsby-plugin-scroll-reveal`,
-    options: {
-        threshold: 0.01
-    }
-    },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-scroll-reveal',
+      options: { threshold: 0.01 }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/assets/images`,
+        name: 'images',
+        path: 'src/assets/images',
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `bitcoin-cash-org`,
-        short_name: `bchorg`,
-        start_url: `/`,
-        background_color: `#10243f`,
-        theme_color: `#10243f`,
-        display: `minimal-ui`,
-        icon: `src/assets/images/bitcoin-cash-logo-white-small.png`, // This path is relative to the root of the site.
+        name: 'bitcoin-cash-org',
+        short_name: 'bchorg',
+        start_url: '/',
+        background_color: '#10243f',
+        theme_color: '#10243f',
+        display: 'minimal-ui',
+        // This path is relative to the root of the site.
+        icon: 'src/assets/images/bitcoin-cash-logo-white-small.png',
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     {
-      resolve: "gatsby-plugin-react-svg",
+      resolve: 'gatsby-plugin-react-svg',
       options: {
-        rule: {
-          include: /assets(\/|\\)icons/
-        }
-      }
+        rule: { include: /assets(\/|\\)icons/ }
+      },
     },
     {
-      resolve: `gatsby-plugin-asset-path`
-    }
+      resolve: 'gatsby-plugin-asset-path'
+    },
   ],
 }
