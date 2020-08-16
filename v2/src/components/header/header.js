c5520ee3346b72c0be3214dd376bb7009be2059a
deadalnix <deadalnix@gmail.com>
2020-08-17 01:13:56 +0200

    Refactor header.js (#519)

--- v2/src/components/header/header.js
+++ v2/src/components/header/header.js
@@ -4,35 +4,45 @@ import { useStaticQuery, graphql } from "gatsby"
 import headerStyles from "./header.module.scss"
 import navBarItems from './navBarItems';
 import axios from 'axios';
-import '../../assets/lib/hamburgers.min.css';
+import 'assets/lib/hamburgers.min.css';
 import LivePriceWidget from "../liveprice/live-price-widget";
-import Link from '../../global/link';
+import Link from 'global/link';
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 if (typeof window !== 'undefined') {
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (currentScrollPos <= 1000) return;
-  if (prevScrollpos > currentScrollPos) {
-    let navBar = document.getElementById("navbar")
-    if (navBar) navBar.style.top = "0";
-  } else {
-    let navBar = document.getElementById("navbar")
-    if (navBar) navBar.style.top = "-100px";
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos <= 1000) {
+      return;
+    }
+    if (prevScrollpos > currentScrollPos) {
+      let navBar = document.getElementById("navbar")
+      if (navBar) {
+        navBar.style.top = "0";
+      }
+    } else {
+      let navBar = document.getElementById("navbar")
+      if (navBar) {
+        navBar.style.top = "-100px";
+      }
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
-}
 }
 
 const externalLink = (i, linkText, href) => {
-  return <Link data-sal="slide-down"
-            data-sal-delay={100 + (i * 100)}
-            data-sal-duration="1000"
-            data-sal-easing="ease"
-            className={headerStyles.link}
-            key={i}
-            to={href}>{linkText}</Link>
+  return (
+    <Link data-sal="slide-down"
+      data-sal-delay={100 + (i * 100)}
+      data-sal-duration="1000"
+      data-sal-easing="ease"
+      className={headerStyles.link}
+      key={i}
+      to={href}>
+      {linkText}
+    </Link>
+  )
 }
 
 
@@ -45,8 +55,7 @@ const Header = () => {
   const [isActive, setIsActive] = useState(false);
   const [currentUSDPrice, setCurrentUSDPrice] = useState("-")
   const updateBchPrice = () => {
-    axios.get(bchPriceApi).then((response) =>
-    {
+    axios.get(bchPriceApi).then((response) => {
       if (response.data && response.data.USD) {
         setCurrentUSDPrice(response.data.USD);
       }
@@ -62,62 +71,63 @@ const Header = () => {
   }, [])
 
   const data = useStaticQuery(graphql`
-  query SiteThemeQuery {
-    site {
-      siteMetadata {
-        themeColours {
-          primary_dark,
-          primary_light
+    query SiteThemeQuery {
+      site {
+        siteMetadata {
+          themeColours {
+            primary_dark,
+            primary_light
+          }
         }
       }
     }
-  }
-`)
+  `)
 
-const theme = data.site.siteMetadata.themeColours;
+  const theme = data.site.siteMetadata.themeColours;
 
-return (
-  <>
-  <header
-    id={isActive ? "navbar-mobile" : "navbar"}
-    style={{
-      background: `linear-gradient(270deg, ${theme.primary_dark} 0%, ${theme.primary_light} 100%)`
-    }}
-  >
-    <div className={headerStyles.headerBar}>
-      <div className={headerStyles.headerStart}>
-        <img src={logo} className={headerStyles.logo} alt="bitcoincashlogo"></img>
-        <LivePriceWidget currentPrice={"$" + currentUSDPrice} ticker={"USD"} url={"/buy-bitcoin-cash.html"}></LivePriceWidget>
+  return <>
+    <header
+      id={isActive ? "navbar-mobile" : "navbar"}
+      style={{
+        background: `linear-gradient(270deg, ${theme.primary_dark} 0%, ${theme.primary_light} 100%)`
+      }}
+    >
+      <div className={headerStyles.headerBar}>
+        <div className={headerStyles.headerStart}>
+          <img src={logo} className={headerStyles.logo} alt="bitcoincashlogo" />
+          <LivePriceWidget currentPrice={"$" + currentUSDPrice} ticker={"USD"} url={"/buy-bitcoin-cash.html"} />
+        </div>
+
+        <div className={`${headerStyles.headerLinks} topBotomBordersOut`}>
+          {navBarItems.map((headerLink, index) =>
+            headerLink.href
+              ? externalLink(index, headerLink.text, headerLink.href)
+              : headerLink.dropdown
+          )}
+        </div>
+
+        <div className={headerStyles.mobileHeaderLinks}>
+          <div className={`hamburger hamburger--squeeze ${isActive ? 'is-active' : ''}`} onClick={() => setIsActive(!isActive)}>
+            <div className="hamburger-box">
+              <div className="hamburger-inner" />
+            </div>
+          </div>
+        </div>
+        <div className={headerStyles.mobileMenu} style={isActive ? {height:'calc(100vh - 100px)', backgroundColor: theme.primary_dark} : null}>
+          {isActive &&
+            <div className={headerStyles.mobileNavLinks}>
+              {navBarItems.map(headerLink =>
+                headerLink.href
+                  ? <MobileHeaderLink text={headerLink.text} href={headerLink.href} key={headerLink.text} id={headerLink.index} />
+                  : headerLink.mobileDropdown
+              )}
+            </div>
+          }
+        </div>
+
       </div>
-
-      <div className={`${headerStyles.headerLinks} topBotomBordersOut`}>
-        {navBarItems.map((headerLink, index) => {
-          return headerLink.href ? 
-            externalLink(index, headerLink.text, headerLink.href) : headerLink.dropdown
-        })}
-      </div>
-
-
-      <div className={headerStyles.mobileHeaderLinks}>
-      <div className={`hamburger hamburger--squeeze ${isActive ? 'is-active' : ''}`} onClick={() => setIsActive(!isActive)}>
-        <div className="hamburger-box">
-        <div className="hamburger-inner"></div>
-      </div>
-    </div>
-    </div>
-    <div className={headerStyles.mobileMenu} style={isActive ? {height:'calc(100vh - 100px)', backgroundColor: theme.primary_dark} : null}>
-    {isActive &&
-      <div className={headerStyles.mobileNavLinks}>
-        {navBarItems.map(headerLink => {
-          return headerLink.href ? 
-            <MobileHeaderLink text={headerLink.text} href={headerLink.href} key={headerLink.text} id={headerLink.index}></MobileHeaderLink> : headerLink.mobileDropdown
-        })}
-      </div>}
-    </div>
-   
-    </div>
-  </header>
-  </>)
+    </header>
+  </>
 }
 
 export default Header
