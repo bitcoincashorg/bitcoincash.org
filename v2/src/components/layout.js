8fd7c44fddf1b9bad0c52d4fbf59df188a2b0125
nhutran1201 <61886602+nhutran1201@users.noreply.github.com>
2020-08-17 13:27:10 +0700

    Revert "Fix indentation in layout.js (#515)"
    This reverts commit c4acead08caa784eb38429f6c9f0410bd162af28.

--- v2/src/components/layout.js
+++ v2/src/components/layout.js
@@ -6,17 +6,18 @@
  */
 
 import React from "react"
+import Header from "./header/header"
 import "./layout.scss"
 import "../global/global.scss";
-import Header from "./header/header"
 import Footer from "./footer/footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      <main>{children}</main>
-      <Footer />
+        <main>{children}</main>
+        <Footer>
+        </Footer>
     </>
   )
 }
