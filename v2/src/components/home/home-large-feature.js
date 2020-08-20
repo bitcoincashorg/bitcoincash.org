b13219065ab00b4712c1bb17a1fdd1abf5bdd299
John <jkuney17@gmail.com>
2020-08-20 17:54:04 -0500

    improve translation descriptions

--- v2/src/components/home/home-large-feature.js
+++ v2/src/components/home/home-large-feature.js
@@ -11,28 +11,28 @@ const HomeLargeFeature = () => {
     <LargeFeature
       featureTitle={fbt(
         "Bitcoin Cash does what banks can't do",
-        "Large feature's title"
+        "'does what banks can't do' homepage feature's title"
       )}
       featuredPoints={[
         {
           icon: <PhoneIcon />,
           text: fbt(
             "Payments go directly from one person to another.",
-            "Large feature's point one"
+            "'does what banks can't do' homepage feature's first point"
           ),
         },
         {
           icon: <TinyFees />,
           text: fbt(
             "No matter how much you send, fees are always tiny.",
-            "Large feature's point two"
+            "'does what banks can't do' homepage feature's second point"
           ),
         },
         {
           icon: <SentFast />,
           text: fbt(
             "Money is sent and received fast—no delays, no limits.",
-            "Large feature's point three"
+            "'does what banks can't do' homepage feature's third point"
           ),
         },
       ]}
@@ -40,13 +40,13 @@ const HomeLargeFeature = () => {
         icon: <Anywhere />,
         title: fbt(
           "And everyone, everywhere can use it",
-          "Large feature's CTA title"
+          "'does what banks can't do' homepage feature's CTA title"
         ),
         body: fbt(
           "Bitcoin Cash is sound money for the world. To meet growing demand, developers are always scaling the Bitcoin Cash network.",
-          "Large feature's CTA text"
+          "'does what banks can't do' homepage feature's CTA text"
         ),
-        buttonText: fbt("See the roadmap", "Large feature's CTA button text"),
+        buttonText: fbt("See the roadmap", "'does what banks can't do' homepage feature's CTA button text"),
         btnHref: "/roadmap.html",
       }}
     ></LargeFeature>
