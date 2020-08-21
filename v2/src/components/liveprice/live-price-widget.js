3bf97eafac5c419817a7570d39b4cc9b02c67802
johnkuney <31288518+johnkuney@users.noreply.github.com>
2020-08-21 10:11:15 -0500

    Tweak to price ticker style (#552)
    * Tweaks to price ticker stlye
    
    * remove uneccessary theme query

--- v2/src/components/liveprice/live-price-widget.js
+++ v2/src/components/liveprice/live-price-widget.js
@@ -1,34 +1,12 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 import livePricingStyles from "./live-price-widget.module.css"
 
 const LivePriceWidget = ({ currentPrice, ticker, url }) => {
-  const data = useStaticQuery(graphql`
-    query LivePriceThemeQuery {
-      site {
-        siteMetadata {
-          themeColours {
-            primary_dark
-            primary_light
-          }
-        }
-      }
-    }
-  `)
-
-  const theme = data.site.siteMetadata.themeColours
 
   return (
     <a className={livePricingStyles.widgetContainer} href={url}>
       <div className={livePricingStyles.currentPrice}>{currentPrice}</div>
-      <div className={livePricingStyles.tickerContainer}>
-        <div
-          className={livePricingStyles.ticker}
-          style={{ color: theme.primary_dark }}
-        >
-          {ticker}
-        </div>
-      </div>
+      <div className={livePricingStyles.ticker}>{ticker}</div>
     </a>
   )
 }
