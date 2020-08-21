8dc01cc15d8976316a0ef3d3002f8a1593b092bd
John <jkuney17@gmail.com>
2020-08-20 22:08:51 -0500

    remove uneccessary theme query

--- v2/src/components/liveprice/live-price-widget.js
+++ v2/src/components/liveprice/live-price-widget.js
@@ -1,22 +1,7 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
 import livePricingStyles from "./live-price-widget.module.css"
 
 const LivePriceWidget = ({ currentPrice, ticker, url }) => {
-  const data = useStaticQuery(graphql`
-    query LivePriceThemeQuery {
-      site {
-        siteMetadata {
-          themeColours {
-            primary_dark
-            primary_light
-          }
-        }
-      }
-    }
-  `)
-
-  const theme = data.site.siteMetadata.themeColours
 
   return (
     <a className={livePricingStyles.widgetContainer} href={url}>
