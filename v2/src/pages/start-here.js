40d0d936bde2b7a86f22250a22a6257b89274879
John <jkuney17@gmail.com>
2020-08-26 18:02:10 -0500

    make page

---
+++ v2/src/pages/start-here.js
@@ -0,0 +1,19 @@
+import React from "react"
+import fbt from "fbt"
+import SEO from "components/seo"
+
+
+
+
+
+
+const StartHerePage = () => {
+  return (
+    <>
+      <SEO title="Start Here" />
+      
+    </>
+  )
+}
+
+export default StartHerePage
