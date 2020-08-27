570f682dbd31202bc2a98387c546af3740d74216
John <jkuney17@gmail.com>
2020-08-26 22:25:04 -0500

    add scss module

--- v2/src/pages/start-here.js
+++ v2/src/pages/start-here.js
@@ -1,6 +1,9 @@
 import React from "react"
 import fbt from "fbt"
 import SEO from "components/seo"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import S from './start-here.module.scss';
 
 
 
@@ -11,6 +14,16 @@ const StartHerePage = () => {
   return (
     <>
       <SEO title="Start Here" />
+      <Container>
+        <Row>
+          <Col sm={6} className={S.styles}>
+            content
+          </Col>
+          <Col sm={6} className={S.styles}>
+            content
+          </Col>
+        </Row>
+      </Container>
       
     </>
   )
