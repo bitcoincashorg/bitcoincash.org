f4ffe5ef44abe912bb3b8b51a9fe8bcabbacc134
John <jkuney17@gmail.com>
2020-08-28 12:36:02 -0500

    Reformatting to use react bootstrap accordion

--- v2/src/layout/announcement-bar/announcement-bar.js
+++ v2/src/layout/announcement-bar/announcement-bar.js
@@ -1,36 +1,14 @@
-import React, { useState, useRef } from "react"
+import React from "react"
 import fbt from "fbt"
 import S from "./announcement-bar.module.scss"
-import DismissButton from "assets/images/close.png"
 import PrimaryButton from "components/buttons/primary-button"
 import Link from "global/link"
+import { Accordion } from "react-bootstrap"
 
-const Accordion = () => {
-  const [State, setState] = useState("")
-  const [Height, setHeight] = useState("0px")
-  const [Rotation, applyRotation] = useState("")
-  const [DismissStyle, setDismiss] = useState("")
-
-  const content = useRef(null)
-
-  function toggleAccordion() {
-    setState(State === "" ? "active" : "")
-    setHeight(
-      State === "active" ? "0px" : `${content.current.scrollHeight}px`
-    )
-    applyRotation(
-      State === "active" ? "" : `${S.rotateX}`
-    )
-  }
-
-  function dismissAccordion() {
-    setState(State === "" ? "active" : "")
-    setDismiss(DismissStyle === "active" ? "0px" : "none")
-  }
-
+const AccordionComponent = () => {
   return (
-    <div className={S.accordionSection} style={{ display: `${DismissStyle}` }}>
-      <div className={S.accordion} onClick={toggleAccordion}>
+    <Accordion className={S.accordionSection}>
+      <Accordion.Toggle as={S.accordionSection} eventKey="0">
         <div className={S.accordionTitle}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path d="M186.301 339.893L96 249.461l-32 30.507L186.301 402 448 140.506 416 110z"></path>
@@ -39,82 +17,80 @@ const Accordion = () => {
             Reminder: November 15th, 2020 Planned Network Upgrade
           </fbt>
         </div>
-
-        <img
-          src={DismissButton}
-          className={`${Rotation} ${S.dismiss}`}
-          alt="close"
-          onClick={dismissAccordion}
-        />
-      </div>
-      <div
-        ref={content}
-        style={{ maxHeight: `${Height}` }}
-        className={S.accordionContent}
-      >
-        <div className={S.annoucementPanel}>
-          <div className={S.annoucementPanelInner}>
-            <h4>
-              <fbt desc="Annoucement bar inner headline">
-                November 15th, 2020 Planned Network Upgrade
-              </fbt>
-            </h4>
-            <p>
-              <fbt desc="Annoucement bar paragraph">
-                The Bitcoin Cash network will undergo a protocol upgrade as per
-                the roadmap. Businesses and individuals who use the Bitcoin Cash
-                network should check to ensure that their software is compatible
-                with the upgrade.
-              </fbt>
-            </p>
-            <PrimaryButton
-              noMarginLeft={true}
-              className={S.primaryButton}
-              buttonText={fbt(
-                "See Roadmap",
-                "'See Roadmap' button of the annoucement bar"
-              )}
-              href={"/roadmap"}
-            />
-            <button className={S.panelButton} onClick={toggleAccordion}>
-              <fbt desc="close button">Close</fbt>
-            </button>
-          </div>
-          <div className={`${S.annoucementPanelInner} ${S.centerPanel}`}>
-            <h4>
-              <fbt desc="Annoucement bar 'Compatible Implementations'">
-                Compatible Implementations:
-              </fbt>
-            </h4>
-            <Link
-              className={S.panelLink}
-              href="https://www.bitcoinabc.org/2020-08-18-bitcoin-abc-0-22-0/"
-            >
-              Bitcoin ABC 0.22.x
-            </Link>
-            <h4>
-              <fbt desc="Annoucement bar 'Additional Information:'">
-                Additional Information:
-              </fbt>
-            </h4>
-            <Link className={S.panelLink} href="/spec/2020-11-15-upgrade.html">
-              <fbt desc="Annoucement bar 'Upgrade Specification'">
-                Upgrade Specification
-              </fbt>
-            </Link>
-            <Link
-              className={S.panelLink}
-              href="https://github.com/bitcoincashorg/bitcoincash.org/blob/master/workgroups/wg-testing/2020-11-15_upgrade_testnet.md"
-            >
-              <fbt desc="Annoucement bar 'Testnet Information'">
-                Testnet Information
-              </fbt>
-            </Link>
+      </Accordion.Toggle>
+      <Accordion.Collapse eventKey="0">
+        <div className={S.accordionContent}>
+          <div className={S.annoucementPanel}>
+            <div className={S.annoucementPanelInner}>
+              <h4>
+                <fbt desc="Annoucement bar inner headline">
+                  November 15th, 2020 Planned Network Upgrade
+                </fbt>
+              </h4>
+              <p>
+                <fbt desc="Annoucement bar paragraph">
+                  The Bitcoin Cash network will undergo a protocol upgrade as
+                  per the roadmap. Businesses and individuals who use the
+                  Bitcoin Cash network should check to ensure that their
+                  software is compatible with the upgrade.
+                </fbt>
+              </p>
+              <Accordion.Toggle as={S.primaryButton}>
+                <PrimaryButton
+                  noMarginLeft={true}
+                  className={S.primaryButton}
+                  buttonText={fbt(
+                    "See Roadmap",
+                    "'See Roadmap' button of the annoucement bar"
+                  )}
+                  href={"/roadmap"}
+                />
+              </Accordion.Toggle>
+              <Accordion.Toggle as={S.panelButton}>
+                <button className={S.panelButton}>
+                  <fbt desc="close button">Close</fbt>
+                </button>
+              </Accordion.Toggle>
+            </div>
+            <div className={`${S.annoucementPanelInner} ${S.centerPanel}`}>
+              <h4>
+                <fbt desc="Annoucement bar 'Compatible Implementations'">
+                  Compatible Implementations:
+                </fbt>
+              </h4>
+              <Link
+                className={S.panelLink}
+                href="https://www.bitcoinabc.org/2020-08-18-bitcoin-abc-0-22-0/"
+              >
+                Bitcoin ABC 0.22.x
+              </Link>
+              <h4>
+                <fbt desc="Annoucement bar 'Additional Information:'">
+                  Additional Information:
+                </fbt>
+              </h4>
+              <Link
+                className={S.panelLink}
+                href="/spec/2020-11-15-upgrade.html"
+              >
+                <fbt desc="Annoucement bar 'Upgrade Specification'">
+                  Upgrade Specification
+                </fbt>
+              </Link>
+              <Link
+                className={S.panelLink}
+                href="https://github.com/bitcoincashorg/bitcoincash.org/blob/master/workgroups/wg-testing/2020-11-15_upgrade_testnet.md"
+              >
+                <fbt desc="Annoucement bar 'Testnet Information'">
+                  Testnet Information
+                </fbt>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Accordion.Collapse>
+    </Accordion>
   )
 }
 
-export default Accordion
+export default AccordionComponent
