4d1cafa344720933568e58e50ea95fec8b4b40c8
johnkuney <31288518+johnkuney@users.noreply.github.com>
2020-08-31 20:49:43 -0500

    Featured tiles (#571)
    * add tiles
    * adding featured tile component

---
+++ v2/src/components/featured-tiles/featured-tiles.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
+import tilesStyles from "./featured-tiles.module.css"
+import Link from "global/link.js"
+
+const FeaturedTiles = ({ tiles, md }) => {
+  return (
+    <Container>
+      <Row className={tilesStyles.tilesectionpadding}>
+        {tiles.map(item => (
+          <Col md={md} className={tilesStyles.tile}>
+            <Link to={item.link} target="_blank" rel="noreferrer">
+              <img src={item.img} alt={item.alt} />
+              <p>{item.text}</p>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  )
+}
+
+export default FeaturedTiles
