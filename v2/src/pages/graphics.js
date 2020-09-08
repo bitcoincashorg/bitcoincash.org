cd9ab003dbb9880ea52310db10a0b7e4d0c0a86c
John <jkuney17@gmail.com>
2020-09-08 11:05:06 -0500

    adding images

---
+++ v2/src/pages/graphics.js
@@ -0,0 +1,211 @@
+import React from "react"
+import fbt from "fbt"
+import SEO from "components/seo"
+import { Container, Row, Col } from "react-bootstrap"
+import S from "./graphics.module.scss"
+import Logo from "assets/images/logos/bitcoin-cash-logo.jpg"
+import LogoWhite from "assets/images/logos/bitcoin-cash-logo-white.jpg"
+import LogoFlag from "assets/images/logos/bitcoin-cash-logo-flag.jpg"
+import LogoHz from "assets/images/logos/bitcoin-cash-logo-hz.jpg"
+import LogoHzWhite from "assets/images/logos/bitcoin-cash-logo-hz-white.jpg"
+import LogoSquare from "assets/images/logos/bitcoin-cash-logo-square.jpg"
+
+const GraphicsPage = () => {
+  return (
+    <>
+      <SEO title="Bitcoin Cash Wallets" />
+      <Container>
+        <Row>
+          <Col lg={4} md={6}>
+            <div mdclassName={S.logoCard}>
+              <h6>yo</h6>
+              <img src={Logo} alt="Bitcoin Cash Logo" />
+              <a href="/images/media-kit/3-bitcoin-cash-logo-ot-small.png">
+                <fbt desc="Logos page button for small version of image">
+                  Small (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/3-bitcoin-cash-logo-ot-medium.png">
+                <fbt desc="Logos page button for medium version of image">
+                  Medium (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/3-bitcoin-cash-logo-ot-large.png">
+                <fbt desc="Logos page button for large version of image">
+                  Large (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/3-bitcoin-cash-logo-ot-full.png">
+                <fbt desc="Logos page button for full version of image">
+                  Full (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/3-bitcoin-cash-logo-ot.svg">
+                <fbt desc="Logos page button for vector version of image">
+                  Vector (SVG)
+                </fbt>
+              </a>
+            </div>
+          </Col>
+          <Col lg={4} md={6}>
+            <div mdclassName={S.logoCard}>
+              <h6>yo</h6>
+              <img src={LogoWhite} alt="Bitcoin Cash Logo" />
+              <a href="/images/media-kit/2-bitcoin-cash-logo-wt-small.png">
+                <fbt desc="Logos page button for small version of image">
+                  Small (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/2-bitcoin-cash-logo-wt-medium.png">
+                <fbt desc="Logos page button for medium version of image">
+                  Medium (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/2-bitcoin-cash-logo-wt-large.png">
+                <fbt desc="Logos page button for large version of image">
+                  Large (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/2-bitcoin-cash-logo-wt-full.png">
+                <fbt desc="Logos page button for full version of image">
+                  Full (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/2-bitcoin-cash-logo-wt.svg">
+                <fbt desc="Logos page button for vector version of image">
+                  Vector (SVG)
+                </fbt>
+              </a>
+            </div>
+          </Col>
+          <Col lg={4} md={6}>
+            <div mdclassName={S.logoCard}>
+              <h6>yo</h6>
+              <img src={LogoFlag} alt="Bitcoin Cash Logo" />
+              <a href="/images/media-kit/4-bitcoin-cash-logo-flag-small.png">
+                <fbt desc="Logos page button for small version of image">
+                  Small (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/4-bitcoin-cash-logo-flag-medium.png">
+                <fbt desc="Logos page button for medium version of image">
+                  Medium (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/4-bitcoin-cash-logo-flag-large.png">
+                <fbt desc="Logos page button for large version of image">
+                  Large (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/4-bitcoin-cash-logo-flag-full.png">
+                <fbt desc="Logos page button for full version of image">
+                  Full (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/4-bitcoin-cash-logo-flag.svg">
+                <fbt desc="Logos page button for vector version of image">
+                  Vector (SVG)
+                </fbt>
+              </a>
+            </div>
+          </Col>
+          <Col lg={4} md={6}>
+            <div mdclassName={S.logoCard}>
+              <h6>yo</h6>
+              <img src={LogoHz} alt="Bitcoin Cash Logo" />
+              <a href="/images/media-kit/6-bitcoin-cash-logo-horizontal-small.png">
+                <fbt desc="Logos page button for small version of image">
+                  Small (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/6-bitcoin-cash-logo-horizontal-medium.png">
+                <fbt desc="Logos page button for medium version of image">
+                  Medium (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/6-bitcoin-cash-logo-horizontal-large.png">
+                <fbt desc="Logos page button for large version of image">
+                  Large (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/6-bitcoin-cash-logo-horizontal-full.png">
+                <fbt desc="Logos page button for full version of image">
+                  Full (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/6-bitcoin-cash-logo-horizontal.svg">
+                <fbt desc="Logos page button for vector version of image">
+                  Vector (SVG)
+                </fbt>
+              </a>
+            </div>
+          </Col>
+          <Col lg={4} md={6}>
+            <div mdclassName={S.logoCard}>
+              <h6>yo</h6>
+              <img src={LogoHzWhite} alt="Bitcoin Cash Logo" />
+              <a href="/images/media-kit/5-bitcoin-cash-logo-horizontal-wt-small.png">
+                <fbt desc="Logos page button for small version of image">
+                  Small (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/5-bitcoin-cash-logo-horizontal-wt-medium.png">
+                <fbt desc="Logos page button for medium version of image">
+                  Medium (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/5-bitcoin-cash-logo-horizontal-wt-large.png">
+                <fbt desc="Logos page button for large version of image">
+                  Large (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/5-bitcoin-cash-logo-horizontal-wt-full.png">
+                <fbt desc="Logos page button for full version of image">
+                  Full (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/5-bitcoin-cash-logo-horizontal-wt.svg">
+                <fbt desc="Logos page button for vector version of image">
+                  Vector (SVG)
+                </fbt>
+              </a>
+            </div>
+          </Col>
+          <Col lg={4} md={6}>
+            <div mdclassName={S.logoCard}>
+              <h6>up</h6>
+              <img src={LogoSquare} alt="Bitcoin Cash Logo" />
+              <a href="/images/media-kit/12-bitcoin-cash-square-crop-small.png">
+                <fbt desc="Logos page button for small version of image">
+                  Small (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/12-bitcoin-cash-square-crop-medium.png">
+                <fbt desc="Logos page button for medium version of image">
+                  Medium (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/12-bitcoin-cash-square-crop-large.png">
+                <fbt desc="Logos page button for large version of image">
+                  Large (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/12-bitcoin-cash-square-crop-full.png">
+                <fbt desc="Logos page button for full version of image">
+                  Full (PNG)
+                </fbt>
+              </a>
+              <a href="/images/media-kit/12-bitcoin-cash-square-crop.svg">
+                <fbt desc="Logos page button for vector version of image">
+                  Vector (SVG)
+                </fbt>
+              </a>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+
+export default GraphicsPage
