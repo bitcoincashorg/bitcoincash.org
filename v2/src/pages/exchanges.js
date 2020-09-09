319906670ad71e9abb111ed8fbd1fc2165a95c0a
johnkuney <31288518+johnkuney@users.noreply.github.com>
2020-09-09 17:02:04 -0500

    Exchanges page (#589)
    * exchanges page
    
    * update link

---
+++ v2/src/pages/exchanges.js
@@ -0,0 +1,41 @@
+import React from "react"
+import fbt from "fbt"
+import SEO from "components/seo"
+import { Container } from "react-bootstrap"
+import FeaturedTiles from "components/featured-tiles/featured-tiles.js"
+import Tiles from "components/tiles/tiles.js"
+import * as AllExchanges from "../partners/exchanges/*.js"
+
+const ExchangesPage = () => {
+  const filterExchanges = (array, property) => {
+    return array.reduce(function (newarray, obj) {
+      let key = obj[property]
+      if (key) {
+        newarray.push(obj)
+      }
+      return newarray
+    }, [])
+  }
+
+  const exchanges = Object.values(AllExchanges).map(getExchanges =>
+    getExchanges()
+  )
+  const featuredExchanges = filterExchanges(exchanges, "featured")
+
+  return (
+    <>
+      <SEO title="Bitcoin Cash Exchanges" />
+      <Container>
+        <h2 className="centerh2">
+          <fbt desc="'Exchanges' heading on Exchanges page">
+            Featured Exchanges
+          </fbt>
+        </h2>
+        <FeaturedTiles tiles={featuredExchanges} md={4} />
+        <Tiles tiles={exchanges} md={3} />
+      </Container>
+    </>
+  )
+}
+
+export default ExchangesPage
