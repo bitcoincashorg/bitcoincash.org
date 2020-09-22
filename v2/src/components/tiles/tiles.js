dc9c036ce639a908d6831c7c291b90f89fb5d0f3
John <jkuney17@gmail.com>
2020-09-21 22:55:05 -0500

    css tweaks

--- v2/src/components/tiles/tiles.js
+++ v2/src/components/tiles/tiles.js
@@ -6,7 +6,7 @@ const TilesComponent = ({ tiles, md }) => {
   return (
     <Row>
       {tiles.map(item => (
-        <Col md={md} className={tilesStyles.tile}>
+        <Col xs={6} md={md} className={tilesStyles.tile}>
           <a href={item.link} target="_blank" rel="noreferrer">
             <img src={item.img} alt={item.alt} />
           </a>
