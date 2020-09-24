e1de125de2e02315819b2c789e09c8eab4e16a84
John <jkuney17@gmail.com>
2020-09-24 10:51:31 -0500

    improve 404 page

--- v2/src/pages/404.js
+++ v2/src/pages/404.js
@@ -1,11 +1,24 @@
 import React from "react"
+import fbt from "fbt"
 import SEO from "components/seo"
+import { Container } from "react-bootstrap"
+import PrimaryButton from "components/buttons/primary-button"
 
 const NotFoundPage = () => (
   <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404 Page Not Found" />
+    <Container style={{ paddingTop: "200px", paddingBottom: "200px" }}>
+      <h2>
+        <fbt desc="404 page header">
+          The page you are looking for does not exist.
+        </fbt>
+      </h2>
+      <PrimaryButton
+        noMarginLeft={true}
+        buttonText={fbt("Go to the homepage", "404 page button")}
+        href={"/"}
+      />
+    </Container>
   </>
 )
 
