19293c72781fa0e1554841c8bdd0b802f331bdb5
David <Klakurka@gmail.com>
2020-10-12 21:52:41 -0700

    Added PayButton to Services.

---
+++ v2/src/partners/services/paybutton.js
@@ -0,0 +1,9 @@
+import PaybuttonLogo from "./paybutton.png"
+
+const Paybutton = () => ({
+  link: "https://paybutton.org/",
+  alt: "PayButton",
+  img: PaybuttonLogo,
+})
+
+export default Paybutton
