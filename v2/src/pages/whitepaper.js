c9d547e78c77b0fe5a9bfe40876bf85c035a3e5d
John <jkuney17@gmail.com>
2020-10-14 22:25:51 -0500

    add whitepaper page and translations

---
+++ v2/src/pages/whitepaper.js
@@ -0,0 +1,17 @@
+import React from "react"
+import fbt from "fbt"
+import SEO from "components/seo"
+import { useLocaleContext } from "i18n/provider"
+
+
+const Whitepaper = () => {
+  const { slug } = useLocaleContext()
+  return (
+  <>
+    <SEO title={fbt("Bitcoin Whitepaper", "Whitepaper page SEO title")} />
+      <iframe title="Bitcoin Whitepaper" src={"/bitcoin-" + slug + ".pdf"} width="100%" height="1000px" style={{marginBottom: "0px"}} />
+  </>
+  )
+}
+
+export default Whitepaper
