f1a85af5dab3bd4950325458beac95540843d9d0
John <jkuney17@gmail.com>
2020-10-14 22:28:11 -0500

    run format

--- v2/src/pages/whitepaper.js
+++ v2/src/pages/whitepaper.js
@@ -3,14 +3,19 @@ import fbt from "fbt"
 import SEO from "components/seo"
 import { useLocaleContext } from "i18n/provider"
 
-
 const Whitepaper = () => {
   const { slug } = useLocaleContext()
   return (
-  <>
-    <SEO title={fbt("Bitcoin Whitepaper", "Whitepaper page SEO title")} />
-      <iframe title="Bitcoin Whitepaper" src={"/bitcoin-" + slug + ".pdf"} width="100%" height="1000px" style={{marginBottom: "0px"}} />
-  </>
+    <>
+      <SEO title={fbt("Bitcoin Whitepaper", "Whitepaper page SEO title")} />
+      <iframe
+        title="Bitcoin Whitepaper"
+        src={"/bitcoin-" + slug + ".pdf"}
+        width="100%"
+        height="1000px"
+        style={{ marginBottom: "0px" }}
+      />
+    </>
   )
 }
 
