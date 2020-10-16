6ecf988aae78fa46ff589a1049c9c252a4a669eb
johnkuney <31288518+johnkuney@users.noreply.github.com>
2020-10-16 17:56:05 -0500

    Correct whitepaper file name (#645)
    * fix file name
    
    * variable name change

--- v2/src/pages/whitepaper.js
+++ v2/src/pages/whitepaper.js
@@ -5,12 +5,16 @@ import { useLocaleContext } from "i18n/provider"
 
 const Whitepaper = () => {
   const { slug } = useLocaleContext()
+  let suffix = ""
+  if (slug !== "") {
+    suffix += "-" + slug 
+  }
   return (
     <>
       <SEO title={fbt("Bitcoin Whitepaper", "Whitepaper page SEO title")} />
       <iframe
         title="Bitcoin Whitepaper"
-        src={"/bitcoin-" + slug + ".pdf"}
+        src={"/bitcoin" + suffix + ".pdf#view=FitH"}
         width="100%"
         height="1000px"
         style={{ marginBottom: "0px" }}
