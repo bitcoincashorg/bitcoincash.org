011a2fea1e24cdea5eff6624b3f84a4d25d0782b
johnkuney <31288518+johnkuney@users.noreply.github.com>
2020-10-20 09:29:29 -0500

    Whitepaper page revisions (#647)
    * whitepaper updates
    
    * make set language and english top choices

--- v2/src/pages/whitepaper.js
+++ v2/src/pages/whitepaper.js
@@ -1,24 +1,70 @@
 import React from "react"
 import fbt from "fbt"
 import SEO from "components/seo"
+import S from "./whitepaper.module.scss"
+import { Container, Row, Col } from "react-bootstrap"
 import { useLocaleContext } from "i18n/provider"
+import locales from "i18n/locales"
+import Icon from "assets/icons/wallets/paper-wallet.svg"
+
+const whitepapers = Object.entries(locales).map(([_, locale]) => {
+  let link = "/bitcoin-" + locale.slug + ".pdf"
+  return { label: locale.displayName, href: link }
+})
+
+const OriginalPDF = ({ md }) => {
+  return (
+    <Col md={md} className={`${S.pdflink} ${S.featuredpdflink}`}>
+      <a href={"/bitcoin.pdf"} target="_blank" rel="noreferrer">
+        <Icon />
+        English (
+        <fbt desc="Label for english whitepaper saying its the original">
+          Original
+        </fbt>
+        )
+      </a>
+    </Col>
+  )
+}
 
 const Whitepaper = () => {
-  const { slug } = useLocaleContext()
-  let suffix = ""
-  if (slug !== "") {
-    suffix += "-" + slug 
-  }
+  const { slug, displayName } = useLocaleContext()
   return (
     <>
       <SEO title={fbt("Bitcoin Whitepaper", "Whitepaper page SEO title")} />
-      <iframe
-        title="Bitcoin Whitepaper"
-        src={"/bitcoin" + suffix + ".pdf#view=FitH"}
-        width="100%"
-        height="1000px"
-        style={{ marginBottom: "0px" }}
-      />
+      <Container>
+        <Row>
+          {slug === "" ? (
+            <OriginalPDF md={12} />
+          ) : (
+            <>
+              <Col md={6} className={`${S.pdflink} ${S.featuredpdflink}`}>
+                <a
+                  href={"/bitcoin-" + slug + ".pdf"}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <Icon />
+                  {displayName}
+                </a>
+              </Col>
+              <OriginalPDF md={6} />
+            </>
+          )}
+
+          {whitepapers.map(whitepapers =>
+            whitepapers.label !== "English (US)" &&
+            whitepapers.label !== displayName ? (
+              <Col md={4} className={S.pdflink}>
+                <a href={whitepapers.href} target="_blank" rel="noreferrer">
+                  <Icon />
+                  {whitepapers.label}
+                </a>
+              </Col>
+            ) : null
+          )}
+        </Row>
+      </Container>
     </>
   )
 }
