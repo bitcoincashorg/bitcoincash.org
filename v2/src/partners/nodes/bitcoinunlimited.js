90a47d143976e5a6c7676d1d4504c8469e34242f
Andrea Suisani <sickpig@gmail.com>
2020-11-05 00:12:23 +0100

    Remove Bitcoin Unlimited from the nodes list (#655)

--- v2/src/partners/nodes/bitcoinunlimited.js
+++
@@ -1,9 +0,0 @@
-import BitcoinunlimitedLogo from "./bitcoinunlimited.png"
-
-const Bitcoinunlimited = () => ({
-  link: "https://www.bitcoinunlimited.info/download",
-  alt: "Bitcoin Unlimited",
-  img: BitcoinunlimitedLogo,
-})
-
-export default Bitcoinunlimited
