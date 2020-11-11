537e5d63df42fa4cb3475fca5c3131eafdf41082
johnkuney <31288518+johnkuney@users.noreply.github.com>
2020-11-10 18:41:15 -0600

    updating upgrade info (#660)

--- v2/src/pages/upgrade.js
+++ v2/src/pages/upgrade.js
@@ -2,24 +2,27 @@ import React from "react"
 import fbt from "fbt"
 import SEO from "components/seo"
 import { Container } from "react-bootstrap"
-import PrimaryButton from "components/buttons/primary-button"
 import Link from "global/link"
 import S from "./upgrade.module.scss"
 import UpgradeDate from "global/upgrade-date.js"
 import LinkIcon from "assets/icons/link.svg"
+
+const ImplementationLink = ({ text, href }) => {
+  return (
+    <Link className={S.panelLink} href={href} target="_blank" rel="noreferrer">
+      <LinkIcon />
+      {text}
+    </Link>
+  )
+}
 
 const UpgradePage = () => {
   return (
     <>
       <SEO title={fbt("How to Upgrade", "Upgrade page SEO title")} />
       <Container className={S.linkcontainer}>
-        <h2 className="centerh2">
-          <fbt desc="Upgrade page heading">How to Upgrade</fbt>
-        </h2>
         <h3>
-          <UpgradeDate />
-        </h3>
-        <h3>
+          <UpgradeDate /> |{" "}
           <fbt desc="Upgrade page subheadline">Planned Network Upgrade</fbt>
         </h3>
         <p>
@@ -30,51 +33,44 @@ const UpgradePage = () => {
             that their software is compatible with the upgrade.
           </fbt>
         </p>
-        <PrimaryButton
-          noMarginLeft={true}
-          buttonText={fbt(
-            "Prepare for the Upgrade",
-            "Upgrade page 'Prepare for the Upgrade' button"
-          )}
-          href={
-            "https://blog.bitcoinabc.org/2020/09/14/preparing-businesses-for-a-successful-network-upgrade/"
-          }
-        />
-
-        <h4 className={S.header}>
-          <fbt desc="Upgrade page 'Compatible Implementations'">
+        <p>
+          <fbt desc="upgrade page second paragraph">
+            During the network upgrade, a chain split is likely to occur which
+            could result in two chains: BCHA and BCHN.
+          </fbt>
+        </p>
+        <h4>
+          BCHA-
+          <fbt desc="Annoucement bar 'Compatible Implementations'">
             Compatible Implementations:
           </fbt>
         </h4>
-        <Link href="https://www.bitcoinabc.org/2020-08-18-bitcoin-abc-0-22-0/">
-          <LinkIcon />
-          Bitcoin ABC 0.22.x
-        </Link>
-        <h4 className={S.header}>
-          <fbt desc="Upgrade page 'Additional Information:'">
-            Additional Information:
+        <ImplementationLink
+          text="Bitcoin ABC 0.22.x"
+          href="https://www.bitcoinabc.org/2020-08-18-bitcoin-abc-0-22-0/"
+        />
+        <ImplementationLink
+          text="BCHD 0.17.0 (non-mining)"
+          href="https://github.com/gcash/bchd/releases"
+        />
+        <h4>
+          BCHN-
+          <fbt desc="Annoucement bar 'Compatible Implementations'">
+            Compatible Implementations:
           </fbt>
         </h4>
-        <a href="/upgrade/business-guide.pdf" target="_blank" rel="noreferrer">
-          <LinkIcon />
-          <fbt desc="Upgrade page 'upgrade guide' header">
-            Upgrade: Quick Guide
-          </fbt>
-        </a>
-        <br />
-        <Link href="/spec/2020-11-15-upgrade.html">
-          <LinkIcon />
-          <fbt desc="Upgrade page 'Upgrade Specification'">
-            Upgrade Specification
-          </fbt>
-        </Link>
-        <br />
-        <Link href="https://github.com/bitcoincashorg/bitcoincash.org/blob/master/workgroups/wg-testing/2020-11-15_upgrade_testnet.md">
-          <LinkIcon />
-          <fbt desc="Upgrade page 'Testnet Information'">
-            Testnet Information
-          </fbt>
-        </Link>
+        <ImplementationLink
+          text="Bitcoin ABC 0.22.6+ (BCHN network)"
+          href="https://www.bitcoinabc.org/releases/#0.22.6"
+        />
+        <ImplementationLink
+          text="BCHN 22.x.x"
+          href="https://bitcoincashnode.org/en/download.html"
+        />
+        <ImplementationLink
+          text="BCHD 0.17.0"
+          href="https://github.com/gcash/bchd/releases"
+        />
       </Container>
     </>
   )
